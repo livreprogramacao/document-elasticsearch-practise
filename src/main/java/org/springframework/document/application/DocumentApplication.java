d3d351af29a4c73d3e74e63fc5d2eae452538dd7
Fabio Santos Almeida <fabioalmeida@apoioprodesp.sp.gov.br>
2020-07-17 12:56:00 -0300

    Demo project for Spring Data Elasticsearch.

---
+++ src/main/java/org/springframework/document/application/DocumentApplication.java
@@ -0,0 +1,13 @@
+package org.springframework.document.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/springframework/document/application/RestClientConfig.java
@@ -0,0 +1,21 @@
+package org.springframework.document.application;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+
+@Configuration
+public class RestClientConfig {
+
+    @Bean
+    public RestHighLevelClient elasticsearchClient() {
+
+        final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+                .connectedTo("localhost:9200")
+                .build();
+
+        return RestClients.create(clientConfiguration).rest();
+    }
+}
